c767491c74197adf2e6bad6f932ccf55805910ee
jjswan33 <jjswan33@gmail.com>
2014-02-11 13:50:38 -0500

    testing submit script

--- condor/submit.sh
+++ condor/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
+####### MC Signal
 
 if [ "$1" == true ]; 
 then
-####### MC Signal
 
 echo "Test 1"
 # python singletPrimeSubmit.py --useMC True --sample TpBW --fileList TprimeToBW_750_Madgraph.txt --submit True
@@ -14,13 +14,25 @@ fi
 
 ####### MC Background
 
+if [ "$2" == true ]; 
+then
+
+echo "Test 2"
 # python singletPrimeSubmit.py --useMC True --sample W1Jets --fileList W1Jets_Madgraph.txt --submit True
 # python singletPrimeSubmit.py --useMC True --sample TT_LH --fileList TTJETS_SemiLeptonic_Madgraph.txt --submit True
 
+fi
 
 ####### Data
+
+if [ "$3" == true ]; 
+then
+
+echo "Test 3"
 
 # python singletPrimeSubmit.py --useMC False --sample Mu2012A --fileList singleMu2012A_22Jan2013.txt --json Jan222013ReReco_json.txt --submit True
 # python singletPrimeSubmit.py --useMC False --sample Mu2012B --fileList singleMu2012B_22Jan2013.txt --json Jan222013ReReco_json.txt --submit True
 # python singletPrimeSubmit.py --useMC False --sample Mu2012C --fileList singleMu2012C_22Jan2013.txt --json Jan222013ReReco_json.txt --submit True
 # python singletPrimeSubmit.py --useMC False --sample Mu2012D --fileList singleMu2012D_22Jan2013.txt --json Jan222013ReReco_json.txt --submit True
+
+fi
