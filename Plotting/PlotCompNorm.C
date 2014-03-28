3f1f67efbf939dd9426064e1567048492f1e7da1
jjswan33 <jjswan33@gmail.com>
2014-03-28 11:59:26 -0400

    Add plotting scripts to git repository

---
+++ Plotting/PlotCompNorm.C
@@ -0,0 +1,116 @@
+void PlotComp(TString Var, TString nBins, TString lBin, TString hBin, TString XLabel = "", TString Cuts = "", TString Name = "", bool LegRight = true) {
+
+
+ 	TString P1 = Var+">>p1("+nBins+","+lBin+","+hBin+")";
+ 	TString P2 = Var+">>p2("+nBins+","+lBin+","+hBin+")";
+ 	TString P3 = Var+">>p3("+nBins+","+lBin+","+hBin+")";
+ 	TString P4 = Var+">>p4("+nBins+","+lBin+","+hBin+")";
+ 	TString P5 = Var+">>p5("+nBins+","+lBin+","+hBin+")";
+ 	
+	TFile f1("TpBW750.root");
+	TFile f2("TpTH750.root");
+	TFile f3("TpTZ750.root");
+	TFile f4("TT_LH.root");
+	TFile f5("WJets.root");
+ 	//gStyle->SetOptStat(2211);
+ 	
+ 	//TString weight = "__WEIGHT__*__CORR__*19790";	
+	TTree* t1 = f1.Get("ljmet");
+	TTree* t2 = f2.Get("ljmet");
+	TTree* t3 = f3.Get("ljmet");
+	TTree* t4 = f4.Get("ljmet");
+	TTree* t5 = f5.Get("ljmet");
+
+	TCanvas *C = new TCanvas();
+	C->cd();
+
+	t1->Draw(P1,Cuts);
+	p1->SetLineColor(kRed);
+	p1->Sumw2();
+	p1->SetTitle(0);
+	double S = 1.0/p1->Integral();
+	p1->Scale(S);
+	p1->Draw();
+
+	t2->Draw(P2,Cuts,"same");
+	p2->Sumw2();
+  	double S2 = 1.0/p2->Integral();
+  	p2->Scale(S2);
+  	p2->SetLineColor(4);
+	p2->SetTitle(0);
+
+	t3->Draw(P3,Cuts,"same");
+	p3->Sumw2();
+  	double S3 = 1.0/p3->Integral();
+  	p3->Scale(S3);
+  	p3->SetLineColor(3);
+	p3->SetTitle(0);
+
+	t4->Draw(P4,Cuts,"same");
+	p4->Sumw2();
+  	double S4 = 1.0/p4->Integral();
+  	p4->Scale(S4);
+  	p4->SetLineColor(1);
+  	p4->SetMarkerColor(1);
+  	p4->SetMarkerStyle(20);
+  	p4->SetMarkerSize(0.75);
+	p4->SetTitle(0);
+
+	t5->Draw(P5,Cuts,"same");
+	p5->Sumw2();
+  	double S5 = 1.0/p5->Integral();
+  	p5->Scale(S5);
+  	p5->SetLineColor(2);
+  	p5->SetMarkerColor(2);
+  	p5->SetMarkerStyle(34);
+  	p5->SetMarkerSize(0.75);
+	p5->SetTitle(0);
+
+  	p1->GetXaxis()->SetTitle(XLabel);
+  	p1->GetYaxis()->SetTitle("A.U.");
+  	
+  	vector<double> MaxVec;
+  	MaxVec.push_back(p1->GetMaximum());
+  	MaxVec.push_back(p2->GetMaximum());
+  	MaxVec.push_back(p3->GetMaximum());
+  	MaxVec.push_back(p4->GetMaximum());
+  	MaxVec.push_back(p5->GetMaximum());
+  	  	
+  	double max = 0;
+  	for( int i=0; i<MaxVec.size(); i++){
+  		if(MaxVec[i]>max){
+  			max=MaxVec[i];
+  		}
+  	}
+  	p1->SetMaximum(max*1.2);
+  	p1->SetMinimum(0.0);
+ 	p1->Draw("h");
+ 	p2->Draw("hsame");
+ 	p3->Draw("hsame");
+ 	p4->Draw("psame");	
+ 	p5->Draw("psame");	
+ 	
+	if(LegRight){
+		TLegend *le = new TLegend(0.65,0.9,0.9,0.7,"");
+	}
+	else{
+		TLegend *le = new TLegend(0.2,0.9,0.5,0.7,"");
+	}
+	le->AddEntry(p1,"T[750]#rightarrowBW");
+	le->AddEntry(p2,"T[750]#rightarrowtH");
+	le->AddEntry(p3,"T[750]#rightarrowtZ");
+	le->AddEntry(p4,"t#bar{t}");
+	le->AddEntry(p5,"W+Jets");
+	le->SetBorderSize(0);
+	le->SetFillColor(0);
+	le->Draw("same");
+	if(Name == ""){
+		C->SaveAs("PlotsPNG/"+Var+".png");
+		C->SaveAs("PlotsPDF/"+Var+".pdf");
+	}
+	else{
+		C->SaveAs("PlotsPNG/"+Name+".png");
+		C->SaveAs("PlotsPDF/"+Name+".pdf");
+	}		
+	
+}
---
+++ Plotting/Plotter.C
@@ -0,0 +1,531 @@
+makePlot(std::string var, std::string decay, int bins, float min, float max, std::string cut, std::string lumi, std::string chn,TString labelX,TString units = "GeV",bool left=false,bool log = false,bool dndm=false,bool doRatio = false)
+{
+
+	makeDataCard(var, "tmp.root", bins, min, max, cut, lumi, chn, decay);
+	makeStackPlot(TString(var),TString(decay),"tmp.root",labelX,units,left,log,dndm,doRatio);
+
+}
+
+
+makeStackPlot(TString name,TString decay,TString file,TString labelX,TString units = "GeV",bool left=false,bool log = false,bool dndm=false,bool doRatio = false)
+{
+
+  setStyle();
+
+  float xR=0.55;
+  if(left)
+    xR=0.2;
+
+  TString SigLeg = "";
+	if(decay == "TH"){
+		SigLeg = "T`#rightarrow TH";
+	}
+	else if(decay == "TZ"){
+		SigLeg = "T`#rightarrow TZ";
+	}
+	else if(decay == "BW"){
+		SigLeg = "T`#rightarrow BW";
+	}
+	else{
+		std::cout << "Signal decay is undefined, not plotting signal" << std::endl;
+	}
+	
+	
+  if(doRatio){
+  	TCanvas * c = new TCanvas(name,name,600,600);
+  }
+  else{
+  	TCanvas * c = new TCanvas(name,name,600,600);
+    c->SetLeftMargin     (0.18);
+    c->SetRightMargin    (0.05);
+    c->SetTopMargin      (0.08);
+    c->SetBottomMargin   (0.15);
+  }
+
+  c->cd();
+  
+  if(doRatio){
+  	TPad * plotPad = new TPad("pad1","",0.0,0.3,1.0,1.0);
+  	plotPad->SetLeftMargin  (0.18);
+    plotPad->SetTopMargin   (0.10);
+    plotPad->SetRightMargin (0.07);
+    plotPad->SetBottomMargin(0.03); 
+    TPad * ratioPad = new TPad("pad2","",0.0,0.0,1.0,0.3);
+    ratioPad->SetLeftMargin  (0.18);
+    ratioPad->SetTopMargin   (0.00);
+    ratioPad->SetRightMargin (0.07);
+    ratioPad->SetBottomMargin(0.30);
+    ratioPad->SetGridy(1);
+  }
+  else{
+  	TPad * plotPad = new TPad("pad1","",0.0,0.0,1.0,1.0);
+  	plotPad->SetLeftMargin     (0.18);
+    plotPad->SetRightMargin    (0.05);
+    plotPad->SetTopMargin      (0.08);
+    plotPad->SetBottomMargin   (0.15);
+  }
+  
+  plotPad->Draw();
+  plotPad->cd();
+    
+  TFile *f = new TFile(file);
+
+  TH1F * data = (TH1F*)(f->Get("data"));
+  if (dndm) convertToDNDM(data);
+  applyDATAStyle(data);
+
+//   TH1F * QCD = (TH1F*)(f->Get("QCD"));
+//   if (dndm) convertToDNDM(QCD);
+//   applyStyle(QCD,kMagenta-10,1,1001);
+
+  TH1F * ttbar = (TH1F*)(f->Get("ttbar"));
+  if (dndm) convertToDNDM(ttbar);
+  applyStyle(ttbar,kOrange-4,1,1001);
+
+  TH1F * stop = (TH1F*)(f->Get("singletop"));
+  if (dndm) convertToDNDM(stop);
+  applyStyle(singletop,kAzure+2,1,1001);
+
+
+  TH1F * EWK = (TH1F*)(f->Get("wjets"));
+  // EWK->Add((TH1F*)(f->Get(dir+"/VV")));
+  if (dndm) convertToDNDM(EWK);
+  applyStyle(EWK,kRed+2,1,1001);
+
+  if(SigLeg != ""){
+	  TH1F *signal = (TH1F*)(f->Get("TH_750"));
+	  if (dndm) convertToDNDM(signal);
+	  applySignalStyle(signal);
+  }
+
+  THStack *hs = new THStack("hs","");
+  TLegend *l = new TLegend(xR,0.6,xR+0.5,0.9);
+
+  l->AddEntry(data,"Observed","P");
+
+  l->AddEntry(EWK,"W+Jets","F");
+  // l->AddEntry(QCD,"QCD","F");
+  l->AddEntry(ttbar,"t#bar{t}","F");
+  l->AddEntry(stop,"Single t","F");
+
+  if(SigLeg != ""){
+	  if(log){
+		l->AddEntry(signal,SigLeg,"L");
+	  }
+	  else{
+		l->AddEntry(signal,SigLeg,"F");
+	  }
+  }
+    
+  // hs->Add(QCD);
+  hs->Add(EWK);
+  hs->Add(stop);
+  hs->Add(ttbar);
+
+  hs->Draw("HIST");
+  if(data->GetMaximum()*1.2+sqrt(data->GetMaximum())>hs->GetMaximum()) {
+    float max=0.0;
+    if(data->GetMaximum()>hs->GetMaximum())
+      max=data->GetMaximum();
+    else
+      max=hs->GetMaximum();
+
+    if(log) {
+      max*=10;
+      hs->SetMinimum(0.5);
+    }
+    hs->SetMaximum(max*1.5+sqrt(data->GetMaximum()));
+  }
+
+  if(dndm)       hs->SetMinimum(0.001);
+
+  hs->Draw("HIST");
+  if(doRatio){
+  	hs->GetXaxis()->SetLabelSize(0);
+  }
+  else
+  {
+	  if(units!="")
+		hs->GetXaxis()->SetTitle(labelX+" ["+units+"]");
+	  else
+		hs->GetXaxis()->SetTitle(labelX);
+  }
+
+  hs->GetYaxis()->SetTitle("Events");
+  if(dndm)
+    hs->GetYaxis()->SetTitle("dN/d"+labelX);
+
+  if(SigLeg != "")
+  	signal->Draw("HIST,SAME");
+  data->Draw("e,SAME");
+  
+  c->cd();
+  
+  if(doRatio){
+    
+    TH1F * data2 = (TH1F*)data->Clone("data");
+    TH1F * mc = (TH1F*)(ttbar);
+    //mc->Add(QCD);
+    mc->Add(EWK);
+    mc->Add(stop);
+    
+    double xmin = mc->GetXaxis()->GetXmin();
+    double xmax = mc->GetXaxis()->GetXmax();
+    TLine *line = new TLine(xmin,1.0,xmax,1.0);
+    line->SetLineWidth(3);
+    line->SetLineColor(kBlack);    
+    ratioPad->Draw();
+  	ratioPad->cd();
+  	
+  	data2->Divide(data2,mc);
+    data2->SetMarkerStyle(20);
+    data2->SetTitleSize  (0.12,"Y");
+    data2->SetTitleOffset(0.60,"Y");
+    data2->SetTitleSize  (0.12,"X");
+    data2->SetLabelSize  (0.10,"X");
+    data2->SetLabelSize  (0.08,"Y");
+    data2->GetYaxis()->SetTitleFont(42);
+    data2->GetYaxis()->CenterTitle(kTRUE);
+    data2->GetXaxis()->SetTitleFont(42);
+    data2->GetYaxis()->SetRangeUser(0.8,1.2);
+    data2->GetYaxis()->SetNdivisions(305);
+    data2->GetYaxis()->SetTitle("Data/MC");
+    data2->SetMarkerSize(1);
+    gStyle->SetOptTitle(0);
+
+    if(units!="")
+	  data2->GetXaxis()->SetTitle(labelX+" ["+units+"]");
+    else
+	  data2->GetXaxis()->SetTitle(labelX);
+	  
+    data2->Draw("P");
+    line->Draw();
+        
+  }
+  
+  c->cd();
+  plotPad->cd();  
+
+  l->SetBorderSize(0);
+  l->SetFillColor(0);
+  l->SetFillStyle (0);
+  l->Draw();
+
+  float factor=0.0;
+  if(left) factor = 1./2.2;
+  else factor = 1.05;
+
+  float xL = hs->GetXaxis()->GetXmin()+(hs->GetXaxis()->GetXmax()-hs->GetXaxis()->GetXmin())*xR*factor;
+  float yL = hs->GetMaximum()*0.35;
+
+  if(log)
+  	yL = hs->GetMaximum()*0.35;
+
+  float offsetF=yL-0.1*hs->GetMaximum();
+  float offsetFF=yL-0.2*hs->GetMaximum();
+
+  if(log) {
+    offsetF=yL/2.;
+    offsetFF=yL/5.;
+  }
+  
+  TLatex latex;
+  latex.SetNDC();
+  if(doRatio)
+  	latex.SetTextSize(0.05);
+  else
+  	latex.SetTextSize(0.04);
+  	
+  latex.SetTextAlign(11);
+
+  latex.SetTextFont(42);
+  latex.DrawLatex(0.20,0.94,"CMS Preliminary 2012, 19.3 fb^{-1}, #sqrt{s} = 8 TeV");
+
+ if(log)
+   plotPad->SetLogy();
+
+ // c->RedrawAxis(); 
+  c->SaveAs("Plots/"+name+".png");
+  c->SaveAs("Plots/"+name+".pdf");
+  c->SaveAs("Plots/"+name+".root");
+
+}
+
+
+
+void makeDataCard(std::string var, std::string outfile, int bins, float min, float max, std::string cut, std::string lumi, std::string chn, std::string decay) {
+
+	TFile *f = new TFile(outfile.c_str(),"Recreate");
+	TFile *d;
+	TFile *s;
+	float data;
+	float sig;
+	float ttbar;
+	float wjets;
+	float singletop;
+	
+	std::string weight = "__WEIGHT__*"+lumi;
+	
+	if(chn == "mu"){
+		d = new TFile("mu2012.root");
+		data = makeHistogram("data",d,f,var,cut,bins,min,max);
+		weight = "__WEIGHT__*weight_MuonEff_WprimeCalc*"+lumi;
+	}
+	else if(chn == "ele"){
+		d = new TFile("ele2012.root");
+		data = makeHistogram("data",d,f,var,cut,bins,min,max);
+		weight = "__WEIGHT__*weight_ElectronEff_53x_WprimeCalc*"+lumi;
+	}
+	else{
+		std::cout << "Undefined Channel!! Bad things are about to happen" << std::endl;
+	}
+	
+	if(decay == "TH"){
+		s = new TFile("TpTH750.root");
+		sig = makeHistogram("TH_750",s,f,var,"("+cut+")*"+weight,bins,min,max);
+	}
+	else if(decay == "TZ"){
+		s = new TFile("TpTZ750.root");
+		sig = makeHistogram("TZ_750",s,f,var,"("+cut+")*"+weight,bins,min,max);
+	}
+	else if(decay == "BW"){
+		s = new TFile("TpBW750.root");
+		sig = makeHistogram("BW_750",s,f,var,"("+cut+")*"+weight,bins,min,max);
+	}
+	else{
+		std::cout << "Signal decay is undefined, what stupid thing have you done" << std::endl;
+	}
+	TFile *w = new TFile("WJets.root");
+	wjets = makeHistogram("wjets",w,f,var,"("+cut+")*"+weight,bins,min,max);
+	TFile *tt = new TFile("TT.root");
+	ttbar = makeHistogram("ttbar",tt,f,var,"("+cut+")*"+weight,bins,min,max);
+	TFile *t = new TFile("STOP.root");
+	singletop = makeHistogram("singletop",t,f,var,"("+cut+")*"+weight,bins,min,max);
+	f->Close();
+
+}
+
+float makeHistogram(std::string name, TFile* fIn, TFile* fOut, std::string var, std::string cut, int bins, float min, float max, float scaleFactor = 1.) {
+
+	 TTree* tree = fIn->Get("ljmet");
+	 
+     TH1F *h = new TH1F(name.c_str(),name.c_str(),bins,min,max);
+	 h->Sumw2();
+     tree->Draw((var+">>"+name).c_str(),cut.c_str());
+
+     h->Scale(scaleFactor);
+     
+     fOut->cd();
+
+     Double_t error=0.0;
+     float yield = h->IntegralAndError(1,h->GetNbinsX(),error,"");
+     if(yield == 0){
+     	h->SetBinContent(1,0.00001);
+     }
+     h->Write(h->GetName(),TObject::kOverwrite);
+
+     return yield;
+     
+}
+
+void applyStyle(TH1F* h, Color_t fill, Color_t line,int fillStyle)
+{
+  h->SetFillColor(fill);
+  h->SetLineColor(line);
+  h->SetLineWidth(2);
+  h->SetFillStyle(fillStyle);
+}
+
+
+void applySignalStyle(TH1F* h)
+{
+  h->SetFillStyle(0);
+  h->SetLineWidth(3);
+  h->SetLineColor(kBlue+3);
+  h->SetLineStyle(11);
+}
+
+
+void applyDATAStyle(TH1F* h)
+{
+  h->SetMarkerStyle(20.);
+  h->SetMarkerSize (1.3);
+  h->SetLineWidth  ( 3.);
+
+}
+
+void convertToDNDM(TH1F* histo) {
+  for(int i=1;i<=histo->GetNbinsX();++i) {
+    histo->SetBinContent(i,histo->GetBinContent(i)/histo->GetXaxis()->GetBinWidth(i));
+    histo->SetBinError(i,histo->GetBinError(i)/histo->GetXaxis()->GetBinWidth(i));
+  }
+}
+
+
+
+// void scaleHistogram(std::string folder, std::string histo, float scaleFactor)
+//   {
+//     TH1F * h =(TH1F*) fout_->Get((folder+"/"+histo).c_str());
+//     h->Scale(scaleFactor);
+//     fout_->cd(folder.c_str());
+//     h->Write(h->GetName(),TObject::kOverwrite);
+//     fout_->cd();
+//     for(unsigned int i=0;i<shifts_.size();++i) {
+//       TH1F * hh =(TH1F*) fout_->Get((folder+"/"+histo+"_"+shiftsPostFix_[i]+"Up").c_str());
+//       if(hh!=0) {
+// 
+// 	hh->Scale(scaleFactor);
+// 	fout_->cd(folder.c_str());
+// 	hh->Write(hh->GetName(),TObject::kOverwrite);
+// 	fout_->cd();
+// 
+//       }
+// 
+//       TH1F * hhh =(TH1F*) fout_->Get((folder+"/"+histo+"_"+shiftsPostFix_[i]+"Down").c_str());
+//       if(hhh!=0) {
+// 	hhh->Scale(scaleFactor);
+// 	fout_->cd(folder.c_str());
+// 	hhh->Write(hhh->GetName(),TObject::kOverwrite);
+// 	fout_->cd();
+//       }
+//       
+//     }
+// }
+// 
+// 
+// void mergeHistogram(std::string folder, std::string histo1, std::string histo2, std::string newName)
+//   {
+//     TH1F * h1 =(TH1F*) fout_->Get((folder+"/"+histo1).c_str());
+//     TH1F * h2 =(TH1F*) fout_->Get((folder+"/"+histo2).c_str());
+//   	TH1F *hNew =(TH1F*) h1->Clone(TString(newName));
+//     hNew->Add(h2,1.);
+// 
+//     for(int i=1;i<h1->GetNbinsX();++i){
+//       if(h1->GetBinContent(i)<0){
+// 		h1->SetBinContent(i,0.00001);
+// 		h1->SetBinError(i,1.0);
+// 	  }
+// 	}
+// 
+//     fout_->cd(folder.c_str());
+//     hNew->Write(TString(newName),TObject::kOverwrite);
+// }
+//  
+// 
+// void subtractHistogram(std::string folder, std::string histo1, std::string histo2)
+//   {
+//     TH1F * h1 =(TH1F*) fout_->Get((folder+"/"+histo1).c_str());
+//     TH1F * h2 =(TH1F*) fout_->Get((folder+"/"+histo2).c_str());
+//     h1->Add(h2,-1.);
+// 
+//     for(int i=1;i<h1->GetNbinsX();++i){
+//       if(h1->GetBinContent(i)<0){
+// 		h1->SetBinContent(i,0.00001);
+// 		h1->SetBinError(i,1.0);
+// 	  }
+// 	}
+// 
+//     fout_->cd(folder.c_str());
+//     h1->Write(h1->GetName(),TObject::kOverwrite);
+// }
+//   
+// void addHistogram(std::string folder, std::string histo1, std::string histo2)
+//   {
+//     TH1F * h1 =(TH1F*) fout_->Get((folder+"/"+histo1).c_str());
+//     TH1F * h2 =(TH1F*) fout_->Get((folder+"/"+histo2).c_str());
+//     h1->Add(h2,1.);
+// 
+//     fout_->cd(folder.c_str());
+//     h1->Write(h1->GetName(),TObject::kOverwrite);
+// }
+
+
+setStyle()
+{
+  TStyle *tdrStyle = new TStyle("tdrStyle", "Style for P-TDR");
+
+  // Canvas
+  tdrStyle->SetCanvasColor     (0);
+  tdrStyle->SetCanvasBorderSize(10);
+  tdrStyle->SetCanvasBorderMode(0);
+  tdrStyle->SetCanvasDefH      (700);
+  tdrStyle->SetCanvasDefW      (700);
+  tdrStyle->SetCanvasDefX      (100);
+  tdrStyle->SetCanvasDefY      (100);
+
+  // color palette for 2D temperature plots
+  tdrStyle->SetPalette(1,0);
+  
+  // Pads
+  tdrStyle->SetPadColor       (0);
+  tdrStyle->SetPadBorderSize  (10);
+  tdrStyle->SetPadBorderMode  (0);
+  tdrStyle->SetPadBottomMargin(0.15);
+  tdrStyle->SetPadTopMargin   (0.05);
+  tdrStyle->SetPadLeftMargin  (0.18);
+  tdrStyle->SetPadRightMargin (0.07);
+  tdrStyle->SetPadGridX       (0);
+  tdrStyle->SetPadGridY       (0);
+  tdrStyle->SetPadTickX       (1);
+  tdrStyle->SetPadTickY       (1);
+
+  // Frames
+  tdrStyle->SetLineWidth(3);
+  tdrStyle->SetFrameFillStyle ( 0);
+  tdrStyle->SetFrameFillColor ( 0);
+  tdrStyle->SetFrameLineColor ( 1);
+  tdrStyle->SetFrameLineStyle ( 0);
+  tdrStyle->SetFrameLineWidth ( 2);
+  tdrStyle->SetFrameBorderSize(10);
+  tdrStyle->SetFrameBorderMode( 0);
+
+  // Histograms
+  tdrStyle->SetHistFillColor(2);
+  tdrStyle->SetHistFillStyle(0);
+  tdrStyle->SetHistLineColor(1);
+  tdrStyle->SetHistLineStyle(0);
+  tdrStyle->SetHistLineWidth(3);
+  tdrStyle->SetNdivisions(505);
+
+  // Functions
+  tdrStyle->SetFuncColor(1);
+  tdrStyle->SetFuncStyle(0);
+  tdrStyle->SetFuncWidth(2);
+
+  // Various
+  tdrStyle->SetMarkerStyle(20);
+  tdrStyle->SetMarkerColor(kBlack);
+  tdrStyle->SetMarkerSize (1.4);
+
+  tdrStyle->SetTitleBorderSize(0);
+  tdrStyle->SetTitleFillColor (0);
+  tdrStyle->SetTitleX         (0.2);
+
+  tdrStyle->SetTitleSize  (0.055,"X");
+  tdrStyle->SetTitleOffset(1.200,"X");
+  tdrStyle->SetLabelOffset(0.005,"X");
+  tdrStyle->SetLabelSize  (0.050,"X");
+  tdrStyle->SetLabelFont  (42   ,"X");
+
+  tdrStyle->SetStripDecimals(kFALSE);
+
+  tdrStyle->SetTitleSize  (0.055,"Y");
+  tdrStyle->SetTitleOffset(1.600,"Y");
+  tdrStyle->SetLabelOffset(0.010,"Y");
+  tdrStyle->SetLabelSize  (0.050,"Y");
+  tdrStyle->SetLabelFont  (42   ,"Y");
+
+  tdrStyle->SetTextSize   (0.055);
+  tdrStyle->SetTextFont   (42);
+
+  tdrStyle->SetStatFont   (42);
+  tdrStyle->SetTitleFont  (42);
+  tdrStyle->SetTitleFont  (42,"X");
+  tdrStyle->SetTitleFont  (42,"Y");
+
+  tdrStyle->SetOptStat    (0);
+  tdrStyle->SetLineStyleString(11,"20 10");
+
+
+  tdrStyle->cd();
+}
